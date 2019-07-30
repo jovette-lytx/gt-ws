6d919dfd5159d7f880bf0f695008c7575fb4c204
Jovette Estrellado <jovette.estrellado@lytx.com>
2019-07-30 13:55:30 -0700

    Update files

---
+++ mapaddin/js/addin.js
@@ -0,0 +1,58 @@
+geotab.addin.request = (elt, service) => {
+    
+    elt.innerHTML = `
+        <script>
+            geotab.addin.lytxVideoAddIn = function(api, state) {
+                return {
+                    initialize: function(api, state, callback) {
+                        callback();
+                    },
+                    focus: function(api, state) {
+                        let frame = document.getElementById("addinFrame");
+                        window.addEventListener("message", e => {
+                            if (e.data === "getSessionInfo") {
+                                api.getSession(function (session) {
+                                    session["geoTabBaseUrl"] = window.location.hostname;
+                                    frame.contentWindow.postMessage(JSON.stringify(session), "*");
+                                });
+                            }
+                        }, false);
+                    },
+                    blur: function(api, state) {
+                    }
+                }
+            };
+        </script>
+        <div style="height:1000px; width:100%">
+            <iframe id="addinFrame" style="height:100%; width:100%" 
+                    src="https://lytx-geotab-addinservice.stg.ph.lytx.com/authorize.html" ></iframe>
+        </div>`
+    
+    service.api.getSession().then((sessionInfo) => {
+            console.log(sessionInfo)
+    });
+
+    /****
+    let template = (event, data) => {
+        var div = document.createElement("DIV");
+        div.innerHTML = `<strong>Event:</strong> ${ event }, <strong>data</strong>: ${ JSON.stringify(data) }`;
+        elt.appendChild(div);
+    }
+
+    // subscribe to any mouseover events. Will be fired when user pointer over: device, zone, route.
+    // e parameter looks like: {"type":"zone","entity":{"id":"b3C3F"}}
+    service.events.attach('over', (e) => { template('over', e); });
+
+    // subscribe to any mouseout events. Will be fired when user pointer out of: device, zone, route.
+    // e parameter looks like: {"type":"device","entity":{"id":"b2"}}
+    service.events.attach('out', (e) => { template('out', e); });
+
+    // subscribe to any click events. Will be fired when user clicks on: device, zone, route, map.
+    // e parameter looks like: {"type":"zone","entity":{"id":"b3C3F"},"x":1139,"y":282}
+    service.events.attach('click', (e) => { template('click', e); });
+
+    // subscribe to any move events over map.
+    // e parameter looks like: {"x":485,"y":205}
+    //service.events.attach('move', (e) => { template('move', e); });
+    ****/
+};
---
+++ mapaddin/js/customButton.js
@@ -0,0 +1,4 @@
+geotab.customButtons.customExample1 = (event, api, state) => {
+        alert("Custom button Add-In clicked!");
+        state.gotoPage("map");
+};
