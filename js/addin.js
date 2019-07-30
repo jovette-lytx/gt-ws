8744a3edc4037196ea7d41b88e1e23f18027f2c0
jovette-lytx <52802636+jovette-lytx@users.noreply.github.com>
2019-07-29 17:37:05 -0700

    Update addin.js

--- js/addin.js
+++ js/addin.js
@@ -1,43 +1,32 @@
 geotab.addin.request = (elt, service) => {
     
     elt.innerHTML = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
-            <title>Lytx GeoTab AddIn</title>
-            <script>
-                geotab.addin.lytxVideoAddIn = function(api, state) {
-                    return {
-                        initialize: function(api, state, callback) {
-                            callback();
-                        },
-                        focus: function(api, state) {
-                            let frame = document.getElementById("addinFrame");
-                            window.addEventListener("message", e => {
-                                if (e.data === "getSessionInfo") {
-                                    api.getSession(function (session) {
-                                        session["geoTabBaseUrl"] = window.location.hostname;
-                                        frame.contentWindow.postMessage(JSON.stringify(session), "*");
-                                    });
-                                }
-                            }, false);
-                        },
-                        blur: function(api, state) {
-                        }
+        <script>
+            geotab.addin.lytxVideoAddIn = function(api, state) {
+                return {
+                    initialize: function(api, state, callback) {
+                        callback();
+                    },
+                    focus: function(api, state) {
+                        let frame = document.getElementById("addinFrame");
+                        window.addEventListener("message", e => {
+                            if (e.data === "getSessionInfo") {
+                                api.getSession(function (session) {
+                                    session["geoTabBaseUrl"] = window.location.hostname;
+                                    frame.contentWindow.postMessage(JSON.stringify(session), "*");
+                                });
+                            }
+                        }, false);
+                    },
+                    blur: function(api, state) {
                     }
-                };
-            </script>
-
-        </head>
-        <body>
-            <div style="height:100%; width:100%">
-                <iframe id="addinFrame" style="height:100%; width:100%" 
-                        src="https://lytx-geotab-addinservice.stg.ph.lytx.com/authorize.html" ></iframe>
-            </div>
-        </body>
-        </html>`
+                }
+            };
+        </script>
+        <div style="height:100%; width:100%">
+            <iframe id="addinFrame" style="height:100%; width:100%" 
+                    src="https://lytx-geotab-addinservice.stg.ph.lytx.com/authorize.html" ></iframe>
+        </div>`
     
     /****
     let template = (event, data) => {
