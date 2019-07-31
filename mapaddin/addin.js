e8b0845c5296ded63421259c241a80260eba1b88
Jovette Estrellado <jovette.estrellado@lytx.com>
2019-07-30 17:16:35 -0700

    Update addin.js

--- mapaddin/addin.js
+++ mapaddin/addin.js
@@ -23,7 +23,7 @@ geotab.addin.request = (elt, service) => {
                 }
             };
         </script>
-        <div style="height:1000px; width:100%">
+        <div style="height:100%; width:100%">
             <iframe id="addinFrame" style="height:100%; width:100%" 
                     src="https://jovette-lytx.github.io/gt-ws/mapaddin/authorize.html" ></iframe>
         </div>`
