54f1783db1eff7f5096ebe494c13785f6db6eefd
Jovette Estrellado <jovette.estrellado@lytx.com>
2019-07-31 10:41:49 -0700

    Update files

--- mapaddin/addin.js
+++ mapaddin/addin.js
@@ -2,12 +2,15 @@ geotab.addin.request = (elt, service) => {
     
     elt.innerHTML = `
         <script>
+            var baseUrl = "";
             geotab.addin.lytxVideoAddIn = function(api, state) {
                 return {
                     initialize: function(api, state, callback) {
+                        console.log("Initializing Lytx Video Add-In");
                         callback();
                     },
                     focus: function(api, state) {
+                        console.log("Lytx Video Add-In in focus");
                         let frame = document.getElementById("addinFrame");
                         window.addEventListener("message", e => {
                             if (e.data === "getSessionInfo") {
@@ -19,11 +22,13 @@ geotab.addin.request = (elt, service) => {
                         }, false);
                     },
                     blur: function(api, state) {
+                        console.log("Lytx Video Add-In blur");
                     }
                 }
             };
         </script>
-        <div style="height:100%; width:100%">
+
+        <div style="height:150%; width:100%">
             <iframe id="addinFrame" style="height:100%; width:100%" 
                     src="https://jovette-lytx.github.io/gt-ws/mapaddin/authorize.html" ></iframe>
         </div>`
@@ -57,103 +62,6 @@ geotab.addin.request = (elt, service) => {
     // subscribe to any move events over map.
     // e parameter looks like: {"x":485,"y":205}
     //service.events.attach('move', (e) => { template('move', e); });
-
-    async function getSession() {
-        let sessionObject = getGeoTabSessionDetails();
-    
-        getAuthorization(sessionObject.sessionId, sessionObject.userName,
-            sessionObject.database, sessionObject.domain);
-    }
-    
-    function getAuthorization(sessionId, userName, database, geoTabBaseUrl) {
-        let request = new XMLHttpRequest();
-        request.onload = function () {
-            if (request.readyState === 4) {
-                if (this.status === 200) {
-                    let response = JSON.parse(this.response);
-                    let attributes = [];
-                    let name;
-    
-                    for (name in response) {
-                        if (name !== 'action') {
-                            attributes[name] = response[name]; 
-                        }
-                    }
-                    redirectToLytxPlatformPage(response.action, attributes);
-                } else {
-                    let response;
-                    try {
-                        response = JSON.parse(this.response);
-                    } catch(err) {
-                        response = "Unable to parse response. " + err;
-                    }
-                    redirectOnStatusCode(this.status, response.error);
-                }
-            }
-        };
-        request.open("GET", "/api/authorize?sessionId=" + sessionId +
-            "&username=" + userName + "&databaseName=" + database +
-            "&geoTabBaseUrl=" + geoTabBaseUrl, true);
-        request.send();
-    }
-    
-    function redirectOnStatusCode(statusCode, error) {
-        let errorMessage;
-        let errorType;
-        try {
-            errorType = error.type;
-            errorMessage = error.message;
-        } catch (err) {
-            errorType = "GenericError";
-            errorMessage = "Cannot parse error message";
-        }
-    
-        if (statusCode === 500) {
-            if (errorMessage.includes("Lytx")) {
-                window.location = "/errors/lytx500Error.html";
-            } else if (errorMessage.includes("GeoTab")) {
-                window.location = "/errors/geotab500Error.html";
-            } else {
-                window.location = "/errors/loginError.html";
-            }
-        } else if (statusCode === 401) {
-            if (errorMessage.includes("Lytx")) {
-                window.location = "/errors/lytxAuthError.html";
-            } else if (errorMessage.includes("GeoTab")) {
-                window.location = "/errors/geoTabAuthError.html";
-            } else {
-                window.location = "/errors/loginError.html";
-            }
-        } else if (statusCode === 403) {
-            if (errorType.includes("UserNotAuthorizedToAccessLvsException")) {
-                window.location = "/errors/lytxAccessRestricted.html";
-            } else {
-                window.location = "/errors/loginError.html";
-            }
-        } else {
-            window.location = "/errors/loginError.html";
-        }
-    }
-    
-    function redirectToLytxPlatformPage(action, attributes) {
-        const form = document.createElement('form');
-        form.setAttribute('method', 'post');
-        form.setAttribute('action', action);
-    
-        const hiddenFields = Object.keys(attributes).map(key => {
-            const hiddenField = this.document.createElement('input');
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", attributes[key]);
-    
-            return hiddenField;
-        });
-    
-        hiddenFields.forEach(hf => form.appendChild(hf));
-    
-        this.document.body.appendChild(form);
-        form.submit();
-    }
     
 };
 
