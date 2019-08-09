1d4fb5059ad3a37f4d6b5ff18ae7d668e52a8142
Jovette Estrellado <jovette.estrellado@lytx.com>
2019-08-08 17:05:12 -0700

    Update addin.js

--- mapaddin/addin.js
+++ mapaddin/addin.js
@@ -19,9 +19,19 @@ geotab.addin.request = (elt, service) => {
     let sendMessageToChildIframe = (event, data) => {
         console.log("INFO - postToChildFrame() clicked");
         const iframe = document.getElementById("addinFrame");
+        let poiDetails = JSON.stringify(data);
 
         service.localStorage.get("sessionDetails")
             .then((sessionInfo) => {
+                let vehicleDetails = service.api.call("Get",
+                    {"typeName":"Device",
+                        "resultsLimit":1,
+                        "search":{
+                            "id":poiDetails.entity.id
+                        }
+                    }
+                );
+                sessionInfo["vehicleName"] = vehicleDetails.name;
                 iframe.contentWindow.postMessage(JSON.stringify(sessionInfo), "*");
             });
     }
